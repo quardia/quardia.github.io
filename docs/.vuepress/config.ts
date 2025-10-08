2e5bc69a3617d4b6e45ab0aa347e17f173c0b25d
quardia <2091751219@qq.com>
2025-10-08 21:36:50 +0800

    更多页修改1

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -72,7 +72,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         text: '更多',
         link: '/more/',
         items: [
-          { text: '学习', link: '/pages/f2a556/' },
+          { text: '经典古文鉴赏', link: '/pages/f2a556/' },
           { text: '面试', link: '/pages/aea6571b7a8bae86/' },
           { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
           { text: '实用技巧', link: '/pages/baaa02/' },
